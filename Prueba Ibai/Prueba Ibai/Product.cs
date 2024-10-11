ecc92dca5201d5f364f98665613b01ae2d6b28a9
jorgeesg26 <jsalaba2610@g.educaand.es>
2024-10-11 12:29:34 +0200

    intento webscrapping salguero pokebank

---
+++ Prueba Ibai/Prueba Ibai/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaPlaywright
+{
+    public class Product
+    {
+        public string Name { get; init; }
+        public string Url { get; init; }
+        public decimal Price { get; init; }
+        public Product(string name, string url, decimal price)
+        {
+            Name = name;
+            Url = url;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Name: {Name}");
+            stringBuilder.AppendLine($"Url: {Url}");
+            stringBuilder.AppendLine($"Price: {Price}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- Prueba Ibai/Prueba Ibai/Program.cs
+++ Prueba Ibai/Prueba Ibai/Program.cs
@@ -1,56 +1,49 @@
 ﻿using System.Diagnostics;
+using System.Xml;
 using Microsoft.Playwright;
+using static System.Net.Mime.MediaTypeNames;
 namespace PruebaPlaywright;
+using PruebaPlaywright;
 public class Program
 {
-    public static async Task Main()
+    static async Task Main()
     {
         // Necesario para instalar los navegadores
         Microsoft.Playwright.Program.Main(["install"]);
+
         using IPlaywright playwright = await Playwright.CreateAsync();
         BrowserTypeLaunchOptions options = new BrowserTypeLaunchOptions()
         {
             Headless = false // Se indica falso para poder ver el navegador
         };
+
         await using IBrowser browser = await
         playwright.Chromium.LaunchAsync(options);
         await using IBrowserContext context = await browser.NewContextAsync();
         IPage page = await context.NewPageAsync();
-        // Ir a la página de fnac
-        await page.GotoAsync("https://www.pokemillon.com");
 
-       
-        IElementHandle buscar = await page.QuerySelectorAsync("#shopify-section-header > header > nav > div.d-none.d-lg-flex.mt-0.col-1.flex-wrap.justify-content-end.align-items-center.et-header__icons.et-header_icons-account-enable > div");
-        await buscar.ClickAsync();
+        // Ir a la página de CardMarket
+        await page.GotoAsync("https://pokebank.es");
+
+        //IElementHandle? acceptButton = await page.QuerySelectorAsync("#CookiesConsent > div > div > form > div > button");
+        //if (acceptButton != null) await acceptButton.ClickAsync();
+
         // Escribimos en la barra de búsqueda lo que queremos buscar
-        IElementHandle searchInput = await page.QuerySelectorAsync("#et-search_popup > div > div > div > div.et-search__popup.d-flex.align-items-center > form > input");
-        await searchInput.FillAsync("sobre");
+        // Para conseguir el enlace, inspeccionamos en la zona
+        IElementHandle buscar = await page.QuerySelectorAsync("#menu-1-c9878bc > li.astm-search-menu.is-menu.is-dropdown.menu-item > a > svg > path");
+        await buscar.ClickAsync();
+        IElementHandle searchInput = await page.QuerySelectorAsync("#is-search-input-24930");
+        await searchInput.FillAsync("sobres de pokemon");
+
         // Le damos al botón de buscar
-        IElementHandle searchButton = await page.QuerySelectorAsync("#et-search_popup > div > div > div > div.et-search__popup.d-flex.align-items-center > form > button");
+        IElementHandle searchButton = await page.QuerySelectorAsync("#menu-1-c9878bc > li.astm-search-menu.is-menu.is-dropdown.menu-item > form > button > span.is-search-icon");
         await searchButton.ClickAsync();
         await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+
         // Recorremos la lista de productos y recolectamos los datos
         List<Product> products = new List<Product>();
-        IReadOnlyList<IElementHandle> productElements = await
-        page.QuerySelectorAllAsync("#PageContainer > main > div.container.et-list-view-items");
-        /*
-        int contador = 0;
-        foreach (IElementHandle productElement in productElements)
-        {
-            if (contador >= 10)
-            {
-                break;
-            }
-            try
-            {
-                Product product = await GetProductAsync(productElement);
-                products.Add(product);
-                Console.WriteLine(product);
-            }
-            catch { }
-            contador++;
-        }
-        */
+        IReadOnlyList<IElementHandle> productElements = await page.QuerySelectorAllAsync("#main > div > ul");
+
         for (int i = 0; i <= 9; i++)
         {
             try
@@ -62,55 +55,28 @@ public class Program
             }
             catch { }
         }
-        Console.WriteLine("Los 10 primeros productos encontrados:");
-        foreach (var product in products)
-        {
-            Console.WriteLine(product);
-        }
-        //ProcessStartInfo processInfo = new ProcessStartInfo()
-     
+        // Con los datos recolectados, buscamos el producto más barato
+        Product cheapest = products.MinBy(p => p.Price);
+        Product expensive = products.MaxBy(p => p.Price);
+
+        decimal average = products.Average(p => p.Price);
+
+        Console.WriteLine($"La oferta más barata es: {cheapest}");
+        Console.WriteLine($"La oferta más cara es: {expensive}");
+        Console.WriteLine($"La media de los precios de los productos es: {average}");
+
     }
     private static async Task<Product> GetProductAsync(IElementHandle element)
     {
-        IElementHandle priceElement= await element.QuerySelectorAsync("a:nth-child(2) > div.et-list__price > dl > div.price__regular > dd > span"); ;
-        IElementHandle preciorebaja=await element.QuerySelectorAsync("a:nth-child(3) > div.et-list__price > dl > div.price__sale > dd > span");
-        
-        string priceRaw;
-        decimal price;
-        if (priceElement != null)
-        {
-             
-        
-            priceRaw = await priceElement.InnerTextAsync();
-        }
-        else
-        {
-            
-            
-            priceRaw = await preciorebaja.InnerTextAsync();
-           
-        }
-        priceRaw = priceRaw.Replace("EUR", "",StringComparison.OrdinalIgnoreCase);
+        IElementHandle priceElement = await element.QuerySelectorAsync("li.member-discount.discount-restricted.ast-grid-common-col.ast-full-width.ast-article-post.desktop-align-left.tablet-align-left.mobile-align-left.product.type-product.post-18243.status-publish.instock.product_cat-destacados.product_cat-ingles.product_cat-no-limite-pro.product_cat-sobre.has-post-thumbnail.shipping-taxable.purchasable.product-type-simple > div.astra-shop-summary-wrap > span.price > span > bdi");
+        string priceRaw = await priceElement.InnerTextAsync();
+        priceRaw = priceRaw.Replace(" €", "", StringComparison.OrdinalIgnoreCase);
         priceRaw = priceRaw.Trim();
-        price = decimal.Parse(priceRaw);
-        IElementHandle nameElement = await
-        element.QuerySelectorAsync(".s-item__title");
-        string name = await nameElement.InnerTextAsync();
-        IElementHandle urlElement = await element.QuerySelectorAsync("a");
-        string url = await urlElement.GetAttributeAsync("href");
-        return new Product(name, url, price);
-    }
+        decimal price = decimal.Parse(priceRaw);
 
-}
-public class Product
-{
-    public string Name { get; init; }
-    public string Url { get; init; }
-    public decimal Price { get; init; }
-    public Product(string name, string url, decimal price)
-    {
-        Name = name;
-        Url = url;
-        Price = price;
+        //En la entrega dice que no son necesarios ni el nombre ni la URL del producto, así que ni los llamamos.
+        string name = "";
+        string url = "";
+        return new Product(name, url, price);
     }
 }
